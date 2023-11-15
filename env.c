7f088fd512e956ea69be2ba67c49beb31c39565e
root <samuelserenaluv@gmail.com>
2023-11-15 05:40:27 -0800

    0x16. C - Simple Shell

--- env.c
+++ env.c
@@ -1,149 +1,131 @@
-#include "main.h"
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+int is_name_env(char *token, char *env_name);
+void advance_env(char **env_token);
 /**
- * set_env - sets an env variable.
- * @cmd: command.
- * @env: struc of env variables.
- * Return: nothing.
+ * print_env - Stores and prints the `env`
+ *
+ * @envp: Double pointer to tokens
+ *
+ * Return: 0 (failure) 1 (success)
  */
 
-
-int set_env(char **cmd, env_info *env)
+int print_env(char *envp[])
 {
-	details *nodescanner = env->env_var;
-	char *var, *value, *node;
+	int i, j, char_ctr, buf_ind, check_write;
 
-	var = cmd[1];
-	value = cmd[2];
+	char *buffer;
 
-	if (!value && var)
+	i = j = char_ctr = buf_ind = check_write = 0;
+
+
+	if (!envp)
+		return (0);
+
+	while (envp[i])
 	{
-		env->status = 2;
-                error_messages(env, cmd[0]);
-                env->count++;
-		return (env->status);
+		j = _strlen(envp[i]);
+
+		char_ctr += j + 1;
+		i++;
 	}
 
+	buffer = malloc(sizeof(char) * char_ctr + 1);
+	if (!buffer)
+		return (0);
 
-	else if (var && value && !cmd[3])
+	i = 0;
+	while (envp[i])
 	{
-		if (!nodescanner)
+		j = 0;
+		while (envp[i][j])
+			buffer[buf_ind++] = envp[i][j++];
+
+		buffer[buf_ind++] = '\n';
+		i++;
+	}
+
+	check_write = write(STDOUT_FILENO, buffer, char_ctr);
+	free(buffer);
+	if (check_write < 0)
+		return (0);
+
+	return (1);
+}
+
+/**
+ * _getenv - gets an environmental variable by name
+ *
+ * @env_name: name of environmental variable to find, e.g: PATH, HOME, USER
+ * @environ: array of pointers to environmental variables
+ *
+ * Return: pointer to char after equal sign in env, e.g: PATH= ->/bin:/usr/bin
+ */
+char *_getenv(char *env_name, char **environ)
+{
+	char *ret = NULL;
+	int i = 0;
+
+	if (!environ)
+		return (NULL);
+	while (*environ[i])
+	{
+		if (is_name_env(environ[i], env_name))
 		{
-			addnode(&env->env_var, var, value);
+			ret = environ[i];
+			break;
+		}
+		i++;
+	}
+	/* advance pointer to after ENV=env_var */
+	/*                              ^       */
+	if (ret)
+		advance_env(&ret);
+
+	return (ret);
+}
+/**
+ * is_name_env - checks if a token begins with PATH=
+ * @token: token to check
+ * @env_name: name of environmental variable, e.g: PATH, HOME, etc...
+ *
+ * Return: (1) is path environmental variables, (0) is not path env
+ */
+int is_name_env(char *token, char *env_name)
+{
+	int tok_i = 0, path_i = 0;
+
+	if (!token)
+		return (0);
+	while (token[tok_i] && env_name[path_i])
+	{
+		if (token[tok_i++] != env_name[path_i++])
 			return (0);
-		}
-
-		while (nodescanner)
-		{
-			node = (nodescanner->envar);
-
-			if (!_strcmp(var, node))
-			{
-				(nodescanner->value) = value;
-				return (0);
-			}
-			nodescanner = (nodescanner->next);
-		}
-		if (!nodescanner && _strcmp(var, node))
-			addnode(&(env->env_var), var, value);
 	}
-	else
-	{
-		env->status = 0;
-		error_messages(env, cmd[0]);
-		env->count++;
-	}
-	return (env->status);
+	if (tok_i > 1)
+		return (1);
+	return (0);
 }
 
 /**
- * addnode - adds node at end of linked list.
- * @head: env variables linked list.
- * @var: variable to be set.
- * @value:  value of variable.
- * Return: nothing.
+ * advance_env - takes a pointer to environmental variable and advances
+ * to the character beyond the equal sign.
+ *
+ * @path_env: pointer to the pointer to path token
+ *
+ * Return: always void.
  */
-
-void addnode(details **head, char *var, char *value)
+void advance_env(char **path_env)
 {
-	details  *node;
+	char *token = NULL;
 
-	node = malloc(sizeof(details));
-	if (node)
-	{
-		node->envar = NULL;
-		node->value = NULL;
-		node->next = NULL;
-		if (var && value)
-		{
-			node->envar = string_duplicate(var);
-			if (!node->envar)
-			{
-				free(node);
-				return;
-			}
-			node->value = string_duplicate(value);
-			if (!node->envar)
-			{
-				free(node->envar);
-				free(node);
-				return;
-			}
-		}
-	}
-
-	rearrange(head, node);
-}
-
-/**
- * rearrange - adds node to end of list.
- * @head: double pointer to linked list.
- * @node: node in linked list.
- * Return: nothing.
- */
-
-void rearrange(details **head, details *node)
-{
-	details *end;
-
-
-	if (*head)
-	{
-		end = *head;
-		while (end->next)
-			end = end->next;
-		end->next = node;
-		node->next = NULL;
-	}
-	else
-	{
-		*head = node;
-		node->next = NULL;
-	}
-}
-
-/**
- * reverse_list - reverses linked list.
- * @head: pointer to end of list.
- * Return: Nothing.
- */
-void reverse_list(details **head)
-{
-	details *next, *prevnode = NULL;
-
-	while (*head)
-	{
-
-
-		next = (*head)->next;
-
-		(*head)->next = prevnode;
-
-		prevnode = *head;
-
-		*head = next;
-
-	}
-
-	*head = prevnode;
+	if (!path_env)
+		return;
+	token = *path_env;
+	while (*token != '=')
+		token++;
+	token++; /* set to character beyond equal sign */
+	*path_env = token;
 }
--- exit.c
+++ exit.c
@@ -1,167 +1,22 @@
-#include "main.h"
-#include <stdio.h>
+#include "shell.h"
+#include <stdlib.h>
 
 /**
- * shell_exit - exits the shell.
- * @cmd: command.
- * @env: struct with env varibales.
- * @line: line to free.
- * Return: Nothing:
- */
-int shell_exit(char **cmd, env_info *env, char *line)
-{
-	unsigned long i, exit_value = 0;
-	char *stat = cmd[1];
-
-	if (!stat)
-	{
-		free_chars(line, env);
-		free(cmd);
-		exit(env->status);
-		return (env->status);
-	}
-
-
-	for (i = 0; stat[i]; i++)
-	{
-
-
-		if (stat[i] < '0' || stat[i] > '9' || stat[i] == '-')
-		{
-			env->status = 2;
-			print_error_messages(env, cmd);
-			env->count++;
-			return (2);
-		}
-
-
-
-		exit_value = (exit_value * 10) + stat[i] - '0';
-		if (exit_value > 23456789)
-		{
-			env->status = 2;
-			return (2);
-		}
-	}
-
-	env->status = exit_value;
-	free_chars(line, env);
-	free(cmd);
-	exit(env->status);
-	return (env->status);
-}
-
-
-
-/**
- * echo_exit_status - prints to stdout exit status of last command.
- * @cmd: command
- * @env: struct of env variables.
- * Return: Nothing.
+ * exit_shell - Frees the queue and return exit code
+ *
+ * @q: Pointer to the queue
+ * @status: exit code to exit with
+ * @his_q: history queue to free
+ * @environ: array of pointers to environmental variables
+ *
+ * Return: See macro values
  */
 
-int echo_exit_status(char **cmd __attribute__((unused)), env_info *env)
+void exit_shell(his_q_t *his_q, queue_t *q, int status, char **environ)
 {
-	int digit = env->status, i = 0, j;
-	char *buffer, *stat;
+	write_queue_to_file(his_q, environ);
 
-	if (digit < 0)
-	{
-		i++;
-		digit = -digit;
-	}
-
-	for (; digit; i++)
-		digit /= 10;
-
-
-	buffer = malloc(sizeof(char) * i + 1);
-
-	if (buffer)
-	{
-		stat = itoa(env->status, buffer, env);
-
-		for (j = 0; stat[j]; j++)
-			_putchar(stat[j]);
-
-		_putchar('\n');
-	}
-
-	free(buffer);
-
-	return (env->status);
-}
-/**
- * echo_parser - parses echo command.
- * @cmd: command.
- * @env: struct of shell variables.
- * Return: 0 on success.
- */
-
-int echo_parser(char **cmd, env_info *env)
-{
-	char *parser = cmd[1];
-
-
-	if (_strcmp("$?", parser) == 0 && cmd[1])
-		return (echo_exit_status(cmd, env));
-
-	else if (_strcmp("$$", parser) == 0 && cmd[1])
-		return (echo_pid(cmd, env));
-
-	else
-	{
-		env->status = -1;
-		error_messages(env, cmd[0]);
-		env->count++;
-		return (0);
-	}
-
-	return (0);
-}
-/**
- * echo_pid - prints pid.
- * @cmd: command.
- * @env: struct of shell var.
- * Return: 0 on success.
- */
-
-int echo_pid(char **cmd, env_info *env)
-{
-	int mypid, i, j;
-	char *buffer, *pid;
-
-	mypid = env->pid;
-
-
-	for (i = 0; mypid; i++)
-		mypid /= 10;
-
-
-	buffer = malloc(sizeof(char) * i + 1);
-
-	if (buffer)
-	{
-		mypid = env->pid;
-
-		pid = itoa(mypid, buffer, env);
-
-		for (j = 0; pid[j]; j++)
-			_putchar(pid[j]);
-
-		_putchar('\n');
-		env->status = 0;
-
-	}
-
-	else
-	{
-		env->status = -1;
-		error_messages(env, cmd[0]);
-		env->count++;
-	}
-
-	free(buffer);
-
-	return (0);
+	free_command_queue(q);
+	free_history_queue(his_q);
+	exit(status);
 }
